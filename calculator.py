f297eddb655ee1b4209560a33319e0657d4224d5
Ezra Linnan <ezralinnan@gmail.com>
2025-04-09 15:39:55 -0400

    initial commit

--- calculator.py
+++ calculator.py
@@ -4,8 +4,28 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
+import math
+
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
+
+def log(a, b):
+    try:
+        return math.log(a, b)
+    except ValueError:
+        print("Cannot use log on negative numbers.")
+
+def exp(a, b):
+    return a ** b
